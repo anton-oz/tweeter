03b23b6f14ef44ae923afdaaa46de7efe652e6af
anton <antonosland789@gmail.com>
2024-08-08 18:54:56 -0500

    barebones apollo server added to server and client

--- server/server.js
+++ server/server.js
@@ -2,38 +2,53 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+const { ApolloServer, gql } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const { typeDefs, resolvers } = require('./schemas')
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
-const server = http.createServer(app);
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
 
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000", // Replace with your client URL
-    methods: ["GET", "POST"]
-  }
-});
+async function startApolloServer() {
+  await apolloServer.start();
 
-io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
+  app.use('/graphql', expressMiddleware(apolloServer));
 
-  socket.on("join_room", (room) => {
-    socket.join(room);
-    console.log(`User with ID: ${socket.id} joined room: ${room}`);
+  const server = http.createServer(app);
+
+  const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:3000", // Replace with your client URL
+      methods: ["GET", "POST"]
+    }
   });
 
-  socket.on("send_message", (data) => {
-    const { message, room, user } = data;
-    io.to(room).emit("receive_message", { message, user });
-    console.log(`Message: ${message} sent to room: ${room}`);
+  io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+
+    socket.on("join_room", (room) => {
+      socket.join(room);
+      console.log(`User with ID: ${socket.id} joined room: ${room}`);
+    });
+
+    socket.on("send_message", (data) => {
+      const { message, room, user } = data;
+      io.to(room).emit("receive_message", { message, user });
+      console.log(`Message: ${message} sent to room: ${room}`);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("User disconnected:", socket.id);
+    });
   });
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
+  server.listen(3001, () => {
+    console.log("Server is running on port 3001");
+    console.log('graphql available at http://localhost:3001/graphql');
   });
-});
+};
 
-server.listen(3001, () => {
-  console.log("Server is running on port 3001");
-});
+startApolloServer();
