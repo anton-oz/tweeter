be552db263bd8b631a3ba07acf8108d0a3db5ac0
anton <antonosland789@gmail.com>
2024-08-08 20:01:10 -0500

    correct apollo server uri, started question cycle

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,30 +9,7 @@ function Home() {
   // socket context
   const socket = useSocketContext();
 
-  // State for storing questions
-  // const [question, setQuestions] = useState([]);
-  // const [loading, setLoading] = useState(true);
-  // const [error, setError] = useState(null);
-
-  // // Fetch questions when the component mounts
-  // useEffect(() => {
-  //   const fetchQuestions = async () => {
-  //     try {
-  //       // Fetch the questions
-  //       const fetchedQuestions = await GET_QUESTION();
-  //       setQuestions(fetchedQuestions);
-  //     } catch (err) {
-  //       setError(err.message || 'Failed to fetch questions');
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-
-  //   fetchQuestions();
-  // }, []);
-
-  // if (loading) return <div>Loading...</div>;
-  // if (error) return <div>Error: {error}</div>;
+  
 
   return (
     <>
