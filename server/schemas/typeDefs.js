0331696af74d61e2aeb9abfc589e01d2f828ec56
JustindHolderman <160930788+JustindHolderman@users.noreply.github.com>
2024-08-08 19:13:35 -0500

    Changed name to username throughout server files.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,13 +2,11 @@
 const typeDefs = `
   type Profile {
     _id: ID
-    name: String
+    username: String
     email: String
-    # There is now a field to store the user's password
     password: String
   }
 
-  # Set up an Auth type to handle returning data from a profile creating or user login
   type Auth {
     token: ID!
     profile: Profile
@@ -20,13 +18,11 @@ const typeDefs = `
   }
 
   type Mutation {
-    # Set up mutations to handle creating a profile or logging into a profile and return Auth type
-    addProfile(name: String!, email: String!, password: String!): Auth
+    addProfile(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
 
-    addSkill(profileId: ID!, skill: String!): Profile
+    
     removeProfile(profileId: ID!): Profile
-    removeSkill(profileId: ID!, skill: String!): Profile
   }
 `;
 
