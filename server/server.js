075fe49a2cc15a008f6b263f8818a572f9ef032d
JustindHolderman <160930788+JustindHolderman@users.noreply.github.com>
2024-08-12 21:32:58 -0500

    Removed Global in vite.config

--- server/server.js
+++ server/server.js
@@ -50,10 +50,10 @@ async function startApolloServer() {
   const server = http.createServer(app);
 
   const io = new Server(server, {
-    // cors: {
-    //   origin: "http://localhost:3000", // Replace with your client URL
-    //   methods: ["GET", "POST"]
-    // }
+    cors: {
+      // origin: "http://localhost:3000", // Replace with your client URL
+      methods: ["GET", "POST"]
+    }
   });
 
 
